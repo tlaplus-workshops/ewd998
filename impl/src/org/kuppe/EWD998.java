49e4973c41071ca3407fd8eaa8795f090c6f32b3
Markus Alexander Kuppe <github.com@lemmster.de>
2023-01-24 20:13:58 -0800

    Implementation of EWD998 in Java.
    The implementation took 2h to implement and worked out of the box
    (tested for many hours with 1 to 9 nodes, except the while loop first
    dropped the "tok" message instead of adding it back to inbox at its end.

---
+++ impl/src/org/kuppe/EWD998.java
@@ -0,0 +1,263 @@
+package org.kuppe;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+// https://github.com/tlaplus-workshops/ewd998/blob/main/EWD998.tla
+public class EWD998 {
+
+	public static void main(String[] args) throws Exception {
+		// foo:4711 bar:1234 frob:4423 1
+
+		// foo bar frob
+		final List<String> nodes = Arrays.asList(args).subList(0, args.length - 1);
+		// nodes[1] = bar (zero-indexed)
+		final int myId = Integer.parseInt(args[args.length - 1]);
+
+		new EWD998(nodes, myId, Integer.parseInt(nodes.get(myId).split(":")[1]), myId == 0);
+	}
+
+	private enum Color {
+		black,
+		white
+	}
+	
+	private final Random randomWork = new Random();
+	
+	public EWD998(final List<String> nodes, final int myId, final int port, final boolean isInitiator) throws Exception {
+		final BlockingQueue<String> inbox = new LinkedBlockingQueue<>();
+		
+		/*
+			Init ==
+			    /\ active \in [Node -> BOOLEAN]
+			    /\ pending = [i \in Node |-> 0]
+			    /\ color \in [Node -> Color]
+			    /\ counter = [i \in Node |-> 0]
+			    /\ token = [pos |-> 0, q |-> 0, color |-> "black"]
+		 */
+		boolean active = true;
+		Color color = Color.white;
+		int counter = 0;
+		if (isInitiator) {
+			// /\ token = [pos |-> 0, q |-> 0, color |-> "black"]
+			inbox.put("[tok]0,black");
+		}
+
+		boolean terminationDetected = false;
+		
+		// --------------------------------------------------------------------------------- //
+		
+		// Boilerplate: Network receiver thread (after Init because Init adds an element to inbox).
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		executorService.submit(() -> {
+			try (ServerSocket serverSocket = new ServerSocket(port)) {
+				System.out.printf("Node %s listening on %s\n", myId, serverSocket.getLocalSocketAddress());
+				while (true) {
+					final Socket socket = serverSocket.accept();
+					InputStream inputStream = socket.getInputStream();
+					DataInputStream dataInputStream = new DataInputStream(inputStream);
+					final String msg = dataInputStream.readUTF();
+					
+					System.out.printf("rcv: %s < %s\n", nodes.get(myId), msg);
+					inbox.add(msg);
+					if (msg.startsWith("[trm]")) {
+						// See note at marker "aklseflha" below.
+						dataInputStream.close();
+						inputStream.close();
+						socket.close();
+						// Orderly terminate this executor to not hang indefinitely.
+						executorService.shutdown();
+						return;
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		// --------------------------------------------------------------------------------- //
+		
+		while (true) {
+			final String msg = inbox.take();
+
+			int tokenQ = 0;
+			Color tokenColor = null;
+			
+			// --------------------------------------------------------------------------------- //
+
+			// InitiateToken and PassToken
+			if (msg.startsWith("[tok]")) {
+				// "[tok]-42,black"
+				final String[] qAndColor = msg.substring(5).split(",");
+				tokenQ = Integer.parseInt(qAndColor[0]);
+				tokenColor = Color.valueOf(qAndColor[1]);
+
+				if (isInitiator) {
+					/*
+					InitiateProbe ==
+					    /\ token.pos = 0
+					    /\ \* previous round inconclusive:
+					        \/ token.color = "black"
+					        \/ color[0] = "black"
+					        \/ counter[0] + token.q > 0
+					    /\ ...
+					    /\ UNCHANGED <<active, counter, pending>>                
+
+				    terminationDetected ==
+						  /\ token.pos = 0
+						  /\ token.color = "white"
+						  /\ token.q + counter[0] = 0
+						  /\ color[0] = "white"
+						  /\ ~ active[0]
+					 */
+					terminationDetected = tokenQ + counter == 0 && color == Color.white && tokenColor == Color.white
+							&& !active;
+				}
+			} else if (msg.startsWith("[pl]")) {
+				/*
+					RecvMsg(i) ==
+					    /\ pending[i] > 0
+					    /\ active' = [active EXCEPT ![i] = TRUE]
+					    /\ pending' = [pending EXCEPT ![i] = @ - 1]
+					    /\ counter' = [counter EXCEPT ![i] = @ - 1]
+					    /\ color' = [ color EXCEPT ![i] = "black" ]
+					    /\ UNCHANGED <<token>>
+ 				 */
+				active = true;
+				counter--;
+				color = Color.black;
+			} else if (msg.startsWith("[trm]")) {
+				// (aklseflha) The termination message "[trm]" is *not* part of EWD998. Here,
+				// the initiator sends a trm message to all nodes including itself after
+				// detecting termination. A recipient of a trm message closes its server socket
+				// and orderly shuts down its executor service. Additionally, the trm message is
+				// added to the inbox to cause this thread (main) to also terminate.
+				assert !active;
+				assert inbox.isEmpty();
+				return;
+			} else {
+				throw new IllegalArgumentException("Unknown message type");
+			}
+			
+			// --------------------------------------------------------------------------------- //
+			
+
+			/*
+				SendMsg(i) ==
+				    /\ active[i]
+				    /\ counter' = [counter EXCEPT ![i] = @ + 1]
+				    /\ \E recv \in Node:
+				            pending' = [pending EXCEPT ![recv] = @ + 1]
+				    /\ UNCHANGED <<active, color, token>>
+			 */
+			if (active) {
+				// Simulate some work...
+				Thread.sleep(randomWork.nextInt(6000));
+				if (randomWork.nextBoolean()) {
+					counter++;
+					
+					// \E rcv \in Node: replaced with probabilistic choice.
+					String receiver = nodes.get(randomWork.nextInt(nodes.size()));
+					sendMsg(receiver, "[pl]");
+				}
+			}
+			
+			// --------------------------------------------------------------------------------- //
+				
+			/*
+				Deactivate(i) ==
+				    /\ active[i]
+				    /\ active' = [active EXCEPT ![i] = FALSE]
+				    /\ UNCHANGED <<pending, color, counter, token>>
+			 */
+			if (active) {
+				active = randomWork.nextBoolean();
+			}
+			
+			// --------------------------------------------------------------------------------- //
+			
+			// InitiateToken and PassToken actions
+			// /\ ...
+			// /\ token.pos = i
+			if (tokenColor != null) {
+				if (isInitiator) {
+					/*
+						InitiateProbe ==
+						    /\ ...
+						    /\ token' = [ pos |-> N-1, q |-> 0, color |-> "white"]
+						    /\ color' = [ color EXCEPT ![0] = "white" ]
+						    /\ UNCHANGED <<active, counter, pending>>                
+					 */
+					if (!terminationDetected) {
+						sendMsg(nodes.get(nodes.size() - 1), "[tok]0,white");		
+						color = Color.white;
+					} else {
+						for (String n : nodes) {
+							sendMsg(n, "[trm]");
+						}
+					}
+					tokenColor = null;
+				} else if (!active) {
+					/*
+						PassToken(i) ==
+						    /\ ~ active[i]
+						    /\ token.pos = i
+						    /\ token' = [ token EXCEPT !.pos = @ - 1,
+						                               !.q   = @ + counter[i],
+						                               !.color = IF color[i] = "black" THEN "black" ELSE @ ]
+						    /\ color' = [ color EXCEPT ![i] = "white" ]
+						    /\ UNCHANGED <<active, pending, counter>>
+ 					 */
+					sendMsg(nodes.get(myId - 1), String.format("[tok]%s,%s", tokenQ + counter,
+							color == Color.black ? Color.black : tokenColor));
+					color = Color.white;
+
+					tokenColor = null;
+				} else {
+					// This node owns the token and is active; keep the unchanged token.
+					/*
+						Deactivate(i) ==
+							    /\ ...
+							    /\ UNCHANGED <<..., token>>
+						SendMsg(i) ==
+						    /\ ...
+						    /\ UNCHANGED <<..., token>>
+						RecvMsg(i) ==
+						    /\ ...
+						    /\ UNCHANGED <<token>>
+				     */
+					inbox.add(String.format("[tok]%s,%s", tokenQ, tokenColor));
+				}
+			}
+		}
+	}
+
+	// Boilerplate: Sending messages. 
+	private void sendMsg(String receiver, String msg) throws Exception {
+		System.out.printf("snd: %s < %s\n", receiver, msg);
+		
+		final String[] s = receiver.split(":");
+        final Socket socket = new Socket(s[0], Integer.parseInt(s[1]));
+
+        final OutputStream outputStream = socket.getOutputStream();
+        final DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        dataOutputStream.writeUTF(msg);
+        dataOutputStream.flush();
+        dataOutputStream.close();
+
+        socket.close();
+	}
+}
---
+++ impl/src/org/kuppe/EWD998TestRunner.java
@@ -0,0 +1,47 @@
+package org.kuppe;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class EWD998TestRunner {
+
+	private static final int N = 9;
+	
+	private static final String HOSTNAME = "localhost";
+	private static final int BASEPORT = 4223;
+	
+	public static void main(String[] args) throws Exception {
+		final Random rnd = new Random();
+		
+		while (true) {
+			// Test with n \in 1..N nodes.
+			final int n = rnd.nextInt(N) + 1;
+			
+			// Reversed order starting the initiator node last; initiator would fail when
+			// the receiver isn't up when the initiator sends out the first message.  The
+			// initiator is the last to join and the last to leave the party.
+			final ExecutorService executor = Executors.newFixedThreadPool(n);
+			for (int i = n - 1; i >= 0; i--) {
+
+				final String[] ewd998Args = new String[n + 1];
+				for (int j = 0; j < ewd998Args.length - 1; j++) {
+					ewd998Args[j] = String.format("%s:%s", HOSTNAME, BASEPORT + j);
+				}
+				ewd998Args[ewd998Args.length - 1] = Integer.toString(i);
+				
+				// Cannot .get() the task because the runners would run sequentially and each
+				// run blocks until termination is detected.
+				executor.submit(() -> {
+					EWD998.main(ewd998Args);
+					executor.shutdown();
+					return null;
+				});
+			}
+			
+			// Wait forever for the executor to terminate.
+			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		}
+	}
+}
