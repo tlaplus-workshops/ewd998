bd8415784ab2366847dda1bd42d2dbc1377a71c0
Markus Alexander Kuppe <github.com@lemmster.de>
2023-01-30 17:56:29 -0800

    Remove bogus second dimension from vector clock.
    The TLA+ spec correctly models the vector clock as a function of
    function because variables are global in TLA+.  In an implementation,
    the outer function does not exist explicitly.

--- impl/src/org/kuppe/EWD998VectorClock.java
+++ impl/src/org/kuppe/EWD998VectorClock.java
@@ -7,49 +7,47 @@ import com.google.gson.JsonPrimitive;
 
 public class EWD998VectorClock {
 
-	private final int[][] vc;
+	private final int[] vc;
 	// This node's id.
 	private final int n;
 
 	public EWD998VectorClock(final int n, final int dimension) {
-		this.vc = new int[dimension][dimension];
+		this.vc = new int[dimension];
 		this.n = n;
 	}
 
 	public EWD998VectorClock tick() {
 		// /\ clock' = [ clock EXCEPT ![n][n] = @ + 1 ]
-		this.vc[n][n]++;
+		this.vc[n]++;
 		return this;
 	}
 
 	public void merge(final JsonObject json) {
-		final int j = json.get("id").getAsInt();
-		
-		final JsonArray outer = json.get("clock").getAsJsonArray();
-		final JsonArray inner = outer.get(n).getAsJsonArray();
-
 		/*
-		 * \* This is where the "magic" of the vector clock happens. /\ LET Max(a,b) ==
-		 * IF a < b THEN b ELSE a Merge(r, l) == [ m \in Node |-> IF m = n THEN l[m] + 1
-		 * ELSE Max(r[m], l[m]) ] IN clock' = [ clock EXCEPT ![n] =
-		 * Merge(inbox[n][j].vc, @) ]
+		 * This is where the "magic" of the vector clock happens. 
+		 * 
+		 * /\ LET Max(a,b) == IF a < b THEN b ELSE a
+		 *  
+		 *        Merge(r, l) == [ m \in Node |-> IF m = n THEN l[m] + 1
+		 *                                    ELSE Max(r[m], l[m]) ]
+		 *                                    
+		 *    IN clock' = [ clock EXCEPT ![n] = Merge(inbox[n][j].vc, @) ]
 		 */
-		this.vc[n][j] = Math.max(this.vc[n][j], inner.get(j).getAsInt());
+		final JsonArray arr = json.get("clock").getAsJsonArray();
+		for (int i = 0; i < vc.length; i++) {
+			this.vc[i] = Math.max(this.vc[i], arr.get(i).getAsInt());
+		}
 	}
 
 	public JsonElement toJson() {
 		final JsonObject result = new JsonObject();
 		result.add("id", new JsonPrimitive(n));
 		
-		final JsonArray outer = new JsonArray(vc.length);
+		final JsonArray arr = new JsonArray(vc.length);
 		for (int i = 0; i < vc.length; i++) {
-			JsonArray inner = new JsonArray(vc.length);
-			outer.add(inner);
-			for (int j = 0; j < vc.length; j++) {
-				inner.add(vc[i][j]);
-			}
+			arr.add(vc[i]);
 		}
-		result.add("clock", outer);
+		result.add("clock", arr);
 
 		return result;
 	}
